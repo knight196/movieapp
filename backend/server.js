678b07462a27d992e2ee49786464622d31da7bfc
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-14 15:03:55 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ const authRoutes = require("./routes/routesauth")
 const Userdashboard = require('./Userdashboard/Userorders')
 const Admindashboard = require('./Admindashboard/AdminOrders');
 const nodemailer = require('nodemailer')
-const hbs = require('express-handlebars')
+const hbs = require('nodemailer-express-handlebars')
 
 
 dotenv.config({path:path.resolve(__dirname, './.env')});
