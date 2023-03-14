f31acfb04addd9df71fd629c4cb39a08bf904a0d
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-14 12:37:21 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -8,8 +8,9 @@ const Stripe = require('stripe')
 const authRoutes = require("./routes/routesauth")
 const Userdashboard = require('./Userdashboard/Userorders')
 const Admindashboard = require('./Admindashboard/AdminOrders');
-
-
+const nodemailer = require('nodemailer')
+const hbs = require('nodemailer-express-handlebars')
+const bodyParser = require('body-parser')
 
 dotenv.config({path:path.resolve(__dirname, './.env')});
 
@@ -25,7 +26,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(morgan('dev'))
 app.use('/api/auth', authRoutes)
-
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors())
 
 app.use('/orders', Userdashboard)
@@ -95,6 +97,57 @@ app.post('/api/subscribe', async (req,res)=> {
 })
 
 
+app.post('/api/sendemail', async (req,res) => {
+
+  const {email,paymentId,price,body,date,time} = req.body
+
+
+  try{
+
+    var transporter = nodemailer.createTransport({
+      service:'hotmail',
+      auth:{
+        user:process.env.user,
+        pass:process.env.pass
+      }
+    })
+   
+    const handlebarOptions = {
+  viewEngine:{
+    extName: '.handlebars',
+    partialDir: path.resolve(__dirname,'./views'),
+    defaultLayout:false
+  },
+  viewPath:path.resolve(__dirname,'./views'),
+  extName:'.handlebars'
+}
+
+
+    transporter.use('compile', hbs(handlebarOptions))
+
+    var mailOptions = {
+      from:process.env.user,
+      to:email,
+      subject:'Subscription Confirmation',
+      template:'email',
+      context:{
+      email:email,
+      paymentId:paymentId,
+      price:price,
+      body:body,
+      date:date,
+      time:time
+      }
+    }
+
+    await transporter.sendMail(mailOptions)
+    res.status(200).json({success:true,message:'Email sent'})
+
+  }catch(err){
+    res.status(500).json(err.message)
+  }
+
+})
 
 app.use(express.static(path.join(__dirname, '../frontend/build')))
 app.use('/*', (req,res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')))
