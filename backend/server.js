0113abda45808d7967f452f06812a9d37ee06909
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-14 12:45:12 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -23,8 +23,8 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(morgan('dev'))
 app.use('/api/auth', authRoutes)
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json())
+// app.use(bodyParser.urlencoded({ extended: true }));
 // app.use(cors())
 
 app.use('/orders', Userdashboard)
