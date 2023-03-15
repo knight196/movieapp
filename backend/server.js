1a3a95d1c6b8231aec405a83113a81166ee8dbc6
knight196 <71406824+knight196@users.noreply.github.com>
2023-03-15 10:48:46 +0000

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -113,15 +113,7 @@ app.post('/api/sendemail', async (req,res) => {
       }
     })
    
-    const handlebarOptions = {
-  viewEngine:{
-    extName: '.handlebars',
-    partialDir: path.resolve(__dirname,'./views'),
-    defaultLayout:false
-  },
-  viewPath:path.resolve(__dirname,'./views'),
-  extName:'.handlebars'
-}
+
 
 
     transporter.use('compile', hbs(handlebarOptions))
